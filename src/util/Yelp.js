5b6f74856c3790af3d092ff39c0898cf7551af24
SHUSHU <75408295+lemonshushu@users.noreply.github.com>
2021-04-12 07:39:43 +0900

    Update Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,7 +1,7 @@
 
 const Yelp = {
   search(term, location, sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${process.env.API_KEY}`}})
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${process.env.REACT_API_KEY}`}})
     .then(response => {
       if (response.ok) {
         console.log(response);
